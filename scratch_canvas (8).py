fa931090000040a420ed347e5546ac9822623f0a
MuzainaBatool <95070742+MuzainaBatool@users.noreply.github.com>
2021-12-23 12:45:52 +0530

    Add files via upload

---
+++ scratch_canvas (8).py
@@ -0,0 +1,10 @@
+def neha_muzaina(m):
+    if m % 2==0:
+        print (m,"is an even number")
+    else:
+        print(m,"is an odd number")
+    
+    
+neha_muzaina(8)
+neha_muzaina(5)
+neha_muzaina(89)
